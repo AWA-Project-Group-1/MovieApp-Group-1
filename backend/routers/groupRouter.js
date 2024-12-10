7190a3e4dee636ed7f35d59478bbce4bc2455133
Vo Thanh Nghi Vu <t3voth00@students.oamk.fi>
2024-12-10 14:37:34 +0200

    Update group content

--- backend/routers/groupRouter.js
+++ backend/routers/groupRouter.js
@@ -1,11 +1,7 @@
 import { pool } from '../helpers/db.js';
 import { Router } from 'express';
-//import { authenticate } from '../helpers/authenticate.js';
 import authenticate from '../helpers/authenticate.js';
-//import { auth } from '../helpers/auth.js';
-//import express from 'express';
 import { getGroups, postGroup } from '../controllers/GroupController.js';
-
 
 const router = Router();
 
@@ -18,9 +14,6 @@ router.delete('/delete/:groupId', authenticate, async(req, res) => {
   const {id: userId} = req.user;
     
   console.log("User in request:", req.user);
-  //console.log("Group ID in request:", groupId);
-
-    //if (!userId) {return res.status(401).json({ error: "User not authenticated" });}
     
     try {
       const groupResult = await pool.query(
@@ -49,29 +42,6 @@ router.delete('/delete/:groupId', authenticate, async(req, res) => {
     }
 });
 
-/*
-router.delete('/delete/:id',auth,(req, res, next) => {
-    const id = parseInt(req.params.id)
-    console.log(`Attempting to delete group with ID: ${id}`);
-
-    pool.query('DELETE FROM groupContent WHERE group_id = $1', [id]);
-    pool.query('DELETE FROM groupMembers WHERE group_id = $1', [id]);
-    pool.query('DELETE FROM groups WHERE id = $1', [id], (error, result) => {
-            if (error) {
-                console.error("Database error: ", error);
-                return next(error);
-            }
-
-            if (result.rowCount === 0) {
-                console.error("Group not found for ID:", id);
-                return res.status(404).json({ message: "Group not found" });
-            }
-    
-            console.log("Group deleted successfully.");
-            return res.status(200).json({ message: "Group deleted successfully", id: id });
-    });
-});
-*/
 
 export default router;
 
